b971fd13f5c16e1fda86ef2be2c8a9da2bbf1c1d
slipslack <63278439+slipslack@users.noreply.github.com>
2020-11-25 12:30:29 +1100

    Completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -9,7 +9,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name) 
         {
-            Type = (GradeBookType.Standard);
+            Type = GradeBookType.Standard;
         }
     }
 }
