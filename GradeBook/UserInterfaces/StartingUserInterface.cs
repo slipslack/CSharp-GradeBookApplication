fd2ca3371bfa1f95712af244e172a51ea3282e66
slipslack <63278439+slipslack@users.noreply.github.com>
2020-11-30 14:49:37 +1100

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,21 +40,18 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
+            var type = parts[2];
             if (type == "standard")
-            {
                 gradeBook = new StandardGradeBook(name);
-            }
             if (type == "ranked")
-            {
                 gradeBook = new RankedGradeBook(name);
-            }
             else
             {
-                Console.WriteLine(" is not a supported type of gradebook, please try again", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                 return;
             }
+
 
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
